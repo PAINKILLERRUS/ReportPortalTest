c69efbf3a0bec25bc3c19caff0e0bab564cc8c6c
Ivan Antipov <ivrusbash@gmail.com>
2025-11-16 20:14:11 +0300

    Updated

--- src/test/java/ui/selenide/TestSuite.java
+++ src/test/java/ui/selenide/TestSuite.java
@@ -37,7 +37,7 @@ public abstract sealed class TestSuite permits DashboardUiTest, ApiKeyUiTest {
         Configuration.headless = true;
         Configuration.timeout = 10000;
         Configuration.reopenBrowserOnFail = true;
-        System.setProperty("webdriver.chrome.driver", configReader.getProperty("chromedriver"));
+        //System.setProperty("webdriver.chrome.driver", configReader.getProperty("chromedriver"));
         authorization();
 
         if (isRemote()) {
